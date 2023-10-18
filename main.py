2b605bb5a658ee99936e2af5306c231c898836e2
illiath-tw <146264045+illiath-tw@users.noreply.github.com>
2023-10-18 13:22:15 -0700

    rework begins

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 # import pygame package 
 import pygame
 import pygame.gfxdraw
+import pygame
+import sys
 
-# import init and exit functions
-from exit import exit
-from init import init
+def init():
+    pygame.init()
+    DISPLAY = pygame.display.set_mode((512, 1024))
+    DISPLAY.fill((175, 193, 124))
+    pygame.display.update()
+    pygame.time.delay(10000)
 
-# initialize the window
-def main(): 
-    init()
+def quitmain():
+    pygame.quit()
+    sys.exit()
 
-    # do other things
-    print("working!")   
-    drawgameplayarea()
-    inputchecker()
-
-def inputchecker():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-def gameloop():
-    running = true
-    while running == true:
-        inputchecker()
-    
-
-def drawgameplayarea():
-    bg = pygame.Surface((1920, 1080), flags=0, depth=8)
-    wholescreen = pygame.Rect(0, 0, 1920, 1080)
-    white = pygame.Color(255, 255, 255)
-    pygame.gfxdraw.box(bg, wholescreen, white)
-    
-
-    
-main()
+init()
+quitmain()
